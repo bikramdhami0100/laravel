8279697076136ed1e11b04ad37651be8d604256f
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-03-20 17:21:16 +0545

    this is bikram

---
+++ resources/views/post.blade.php
@@ -0,0 +1,1 @@
+<h1>this is bikram dhami</h1>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('hello', 'post');
+Route::get('/user', function () {
+    return "{name: birkam dhami}";
+});
